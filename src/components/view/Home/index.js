c8f18be38835a53b8e747c5a5c46ec43aa49ec95
muhammadzubairmoosani <muhammadzubairmoosani@gmail.com>
2020-07-30 04:05:58 +0500

    ss

--- src/components/view/Home/index.js
+++ src/components/view/Home/index.js
@@ -1,10 +1,28 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import MainCarousel from "./carousel";
 import AboutUs from "./aboutUs";
 import CardCarouselSegment from "../../common/Carousels/cardCarouselSegment";
-import { Layout } from "../../common";
+import { Layout, Notification } from "../../common";
 import { data } from "../../../util/supplierCardData";
+import { _supplierList } from "../../../service/methods";
+
 const Home = () => {
+  const [list, setList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    _supplierList()
+      .then(({ data }) => {
+        setList(data);
+        setIsLoading(false);
+      })
+      .catch(({ message }) => {
+        setIsLoading(false);
+        Notification.error({ message: message });
+      });
+  }, []);
+
   return (
     <Fragment>
       <MainCarousel />
@@ -12,31 +30,31 @@ const Home = () => {
         <CardCarouselSegment
           heading="19 liters water gallon service"
           subHeading="see all"
-          array={data}
+          array={list}
           route={"/supplier-list"}
         />
         <CardCarouselSegment
           heading="1,000 liters water service"
           subHeading="see all"
-          array={data}
+          array={list}
           route={"/supplier-list"}
         />
         <CardCarouselSegment
           heading="2,000 liters water service"
           subHeading="see all"
-          array={data}
+          array={list}
           route={"/supplier-list"}
         />
         <CardCarouselSegment
           heading="3,000 liters water service"
           subHeading="see all"
-          array={data}
+          array={list}
           route={"/supplier-list"}
         />
         <CardCarouselSegment
           heading="6,000 liters water service"
           subHeading="see all"
-          array={data}
+          array={list}
           route={"/supplier-list"}
         />
       </Layout>
