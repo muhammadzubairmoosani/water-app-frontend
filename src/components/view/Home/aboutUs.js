00417dc3f4a5f4730a26bc2105c21d50ac05d9bf
muhammadzubairmoosani <muhammadzubairmoosani@gmail.com>
2020-08-15 02:22:09 +0500

    as

--- src/components/view/Home/aboutUs.js
+++ src/components/view/Home/aboutUs.js
@@ -4,15 +4,16 @@ import { WallCard } from "../../common";
 const AboutUs = () => {
   return (
     <div className="about_us">
+      <div>
+        <img src={require("../../../assets/images/about-me.webp")} alt="..." />
+      </div>
       <WallCard heading={"About Us"} subHeading={"PANI WALA"}>
         <p>
-          Pani Wala is an on demand Water Tanker Delivery Service that
-          connects you to the Water Tanker Distributor in your neighbourhood.
-          Our aim is to provide a hassle free, high quality water supply at your
-          residences, offices, shops and parties. Pani Wala is all about
-          serving its valued customers by providing easy modes of payment - COD,
-          wallet or monthly payments to choose from. It allows you to schedule
-          the time of delivery.
+          Pani Wala is an on-demand Water Delivery Services that connects you to
+          the Water Distributors in your neighborhood. We aim to provide a
+          hassle-free, high-quality water supply at your residences, offices,
+          shops, and parties. Pani Wala is all about serving its valued
+          customers by providing easy modes of payment - COD.
         </p>
       </WallCard>
     </div>
