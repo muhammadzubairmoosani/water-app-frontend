07dda514c0fbe1d2b3c24e7beca0a3b8688d0c83
muhammadzubairmoosani <muhammadzubairmoosani@gmail.com>
2020-08-15 01:46:39 +0500

    aa

--- src/components/view/Home/carousel.js
+++ src/components/view/Home/carousel.js
@@ -4,32 +4,38 @@ import { Carousel } from "antd";
 const MainCarousel = () => {
   return (
     <div className="main_carousel">
-      <Carousel effect="fade" dotPosition="right" autoplay>
+      <Carousel
+        effect="fade"
+        dotPosition={window.innerWidth >= 600 ? "right" : "bottom"}
+        autoplay
+      >
         <div>
           <img
-            className="img"
             src={require("../../../assets/images/slider1.webp")}
             alt="carousel"
           />
         </div>
         <div>
           <img
-            className="img"
             src={require("../../../assets/images/slider2.webp")}
             alt="carousel"
           />
         </div>
         <div>
           <img
-            className="img"
             src={require("../../../assets/images/slider3.webp")}
             alt="carousel"
           />
         </div>
         <div>
           <img
-            className="img"
             src={require("../../../assets/images/slider4.webp")}
+            alt="carousel"
+          />
+        </div>
+        <div>
+          <img
+            src={require("../../../assets/images/slider5.webp")}
             alt="carousel"
           />
         </div>
