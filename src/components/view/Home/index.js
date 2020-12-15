68b84251c2bbcd5fde46297263ce250f174fb749
muhammadzubairmoosani <muhammadzubairmoosani@gmail.com>
2020-12-15 12:01:44 +0500

    add video

--- src/components/view/Home/index.js
+++ src/components/view/Home/index.js
@@ -3,23 +3,31 @@ import AboutUs from "./aboutUs";
 import Services from "./services";
 import { Layout, Slider } from "../../common";
 
-const images = [
-  require("../../../assets/images/slider1.webp"),
-  require("../../../assets/images/slider2.webp"),
-  require("../../../assets/images/slider3.webp"),
-  require("../../../assets/images/slider4.webp"),
-  require("../../../assets/images/slider5.webp"),
-];
+// const images = [
+//   require("../../../assets/images/slider1.webp"),
+//   require("../../../assets/images/slider2.webp"),
+//   require("../../../assets/images/slider3.webp"),
+//   require("../../../assets/images/slider4.webp"),
+//   require("../../../assets/images/slider5.webp"),
+// ];
 
 export const Home = () => (
   <Fragment>
-    <Slider
+    {/* <Slider
       images={images}
       effect={"fade"}
       dotPosition={window.innerWidth >= 600 ? "right" : "bottom"}
       autoplay
       className="main_carousel"
-    />
+    /> */}
+    <video width="100%" height="100%" muted autoPlay loop>
+      <source
+        src="https://res.cloudinary.com/pani-wala/video/upload/v1608014935/videos/beis5h113ayc1wdlb6hb.mp4"
+        type="video/mp4"
+      />
+      Sorry, your browser doesn't support embedded videos.
+    </video>
+
     <Layout className="card_carousel_container">
       <Services />
     </Layout>
