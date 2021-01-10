3ccb2da51a143de62354f55bc7cc9441a42fdf9a
muhammadzubairmoosani <muhammadzubairmoosani@gmail.com>
2021-01-10 20:24:55 +0500

    asdasd

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 import Cookies from "universal-cookie";
-const cookies = new Cookies();
+// const cookies = new Cookies();
 
 export default axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
-  headers: { token: cookies.get("access_token") },
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 // headers: {
